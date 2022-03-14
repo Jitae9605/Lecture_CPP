9a7ab510dff7b85bc1d1d759e1b1bc39be9d0244
Jitae9605 <dlwlxo3819@naver.com>
2022-03-14 10:14:11 +0900

    2_input.cpp 주석추가

--- chapter1/source/2_input.cpp
+++ chapter1/source/2_input.cpp
@@ -29,18 +29,19 @@ int main(void)
 	std::cout << "두개의 숫자입력: ";
 	std::cin >> val1 >> val2;
 
-	if (val1_2 < val2_2)
+	if (val1_2 < val2_2)							// val1_2 가 val2_2보다 작다면
 	{
 		for (int i = val1_2 + 1; i < val2_2; i++)
 		{
-			result_2 += i;
+			result_2 += i;							// (val1_2 + 1)부터 (val2_2 - 1)까지의 값을  모두 더해 result_2에 저장
 		}
 	}
+
 	else
 	{
-		for (int i = val2_2 + 1; i < val1_2; i++)
+		for (int i = val2_2 + 1; i < val1_2; i++)	// val1_2 가 val2_2보다 크거나 같다면 (그냥 else이므로)
 		{
-			result_2 += i;
+			result_2 += i;							// (val2_2 + 1)부터 (val1_2 - 1)까지의 값을 모두 더해 result_2에 저장
 		}
 
 		std::cout << "두 수사이의 정수 합: " << result_2 << std::endl;
