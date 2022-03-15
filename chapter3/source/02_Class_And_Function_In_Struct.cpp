df99e65040538c16fb18f1c565625f433ba743c1
Jitae9605 <dlwlxo3819@naver.com>
2022-03-15 14:01:54 +0900

    Update chapter3 - Update 02_Class_And_Function_In_Struct.cpp

--- chapter3/source/02_Class_And_Function_In_Struct.cpp
+++ chapter3/source/02_Class_And_Function_In_Struct.cpp
@@ -23,13 +23,23 @@ typedef struct human
 }Human;
 
 // 2)
-class human_class
+class human_class	// 은닉, 캡슐화 - 멤버함수를 통해서만 값을 입력/생성/변경 등이 가능함
 {
-	char name[20];
-	int age;
-	char job[10];
+	char name[20];					// private
+	int age;						// private
+	char job[10];					// private
+	
 
-	void showdata(void)
+	// 생성자 = 초기화시키는 기능을함(현시점에서는)
+	/*human_class (char* a, int b, char* c)
+	{
+		name = a;
+		age = b;
+		job = c;
+	}*/
+
+	// class 내부함수
+	void showdata(void)				// public
 	{
 		cout << "이름 : " << name << endl;
 		cout << "나이 : " << age << "살" << endl;
@@ -53,6 +63,7 @@ int main(void)
 
 	std::cout << "------------ < 2) 클래스의 사용과 구조체와의 차이점  > ----------------" << std::endl;
 	// 2) 클래스의 사용과 구조체와의 차이점
+	human_class h1();
 
 	return 0;
 }
