70ac1c207dc40199a4a5745cf3dc9ea63886e027
Jitae9605 <dlwlxo3819@naver.com>
2022-03-17 17:20:35 +0900

    Update chapter6 - Add 02_Const_Overloading.cpp - Add 03_Friend.cpp - Add 04_Friend_Function.cpp

---
+++ chapter6/source/02_Const_Overloading.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cstring>
+#pragma warning(disable:4996)
+
+// 2. Const 함수 오버로딩
+// 
+
+using namespace std;
+
+class SoSimple
+{
+private:
+	int num;
+public:
+	SoSimple(int n) :num(n)
+	{ }
+
+	SoSimple& AddNum(int n)
+	{
+		num += n;
+		return *this;
+	}
+
+	void SimpleFunc() 
+	{
+		cout << "SimpleFunc : " << num << endl;
+	}
+
+	void SimpleFunc() const
+	{
+		cout << "const SimpleFunc : " << num << endl;
+	}
+};
+
+void YourFunc(const SoSimple &obj)
+{
+	obj.SimpleFunc();
+}
+
+int main()
+{
+	SoSimple obj1(2);
+	const SoSimple obj2(7);
+
+	obj1.SimpleFunc();
+	obj2.SimpleFunc();			// 자동으로 const가 실행
+
+	YourFunc(obj1);				// obj1이 const SoSimple &obj 로 매개변수로 들어가면서 const 함수가 되어 const 함수를 실행함
+	YourFunc(obj2);				
+
+	// *출력결과 *
+	// SimpleFunc : 2
+	// const SimpleFunc : 7
+	// const SimpleFunc : 2
+	// const SimpleFunc : 7
+
+
+	return 0;
+}
---
+++ chapter6/source/04_Friend_Function.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cstring>
+#pragma warning(disable:4996)
+
+// 4. friend 함수 형식/선언
+ 
+
+using namespace std;
+
+class girl;					// 사전선언 ( boy에서 girl을 썼는데 girl이 boy 밑에 있어 인식을 못함 )
+
+class boy
+{
+private:
+	int height;
+	friend class girl;					// girl을 friend로 선언
+public:
+	boy(int len) :height(len)
+	{ }
+	void ShowYourFriendInfo(girl& frn);
+};
+
+class girl
+{
+private:
+	char phNum[20];
+public:
+	girl(const char* num)
+	{
+		strcpy(phNum, num);
+	}
+	void ShowYourFriendInfo(boy& frn);
+	friend class boy;					// boy를 friend로 선언
+
+};
+
+void boy::ShowYourFriendInfo(girl& frn)
+{
+	cout << "Her phone number : " << frn.phNum << endl;
+}
+
+void girl::ShowYourFriendInfo(boy& frn)
+{
+	cout << "His Height : " << frn.height << endl;
+
+}
+
+int main()
+{
+	boy boy(170);
+	girl girl("010-1234-5678");
+	boy.ShowYourFriendInfo(girl);
+	girl.ShowYourFriendInfo(boy);
+
+
+	// * 출력결과 *
+	// Her phone number : 010-1234-5678
+	// His Height : 170
+	return 0;
+}
