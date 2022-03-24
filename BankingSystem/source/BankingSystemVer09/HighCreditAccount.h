70dd17d135e14043f5784796e9a2bde30429173a
Jitae9605 <dlwlxo3819@naver.com>
2022-03-24 12:09:46 +0900

    Update BankingSystem - Add Ver09

--- BankingSystem/source/BankingSystemVer09/HighCreditAccount.h
+++ BankingSystem/source/BankingSystemVer09/HighCreditAccount.h
@@ -8,13 +8,15 @@
 #define __HIGHCREDIT_ACCOUNT_H__
 
 #include"NormalAccount.h"
+#include"String.h"
 
 class HighCreditAccount : public NormalAccount
 {
 private:
 	int specialRate;
 public:
-	HighCreditAccount(int ID, int money, const char* name, int rate, int special)
+	//HighCreditAccount(int ID, int money, const char* name, int rate, int special)
+	HighCreditAccount(int ID, int money, String name, int rate, int special)
 		: NormalAccount(ID, money, name, rate), specialRate(special)
 	{ }
 
